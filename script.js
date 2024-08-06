fbaa853fb70400755a2d54330a0a7ab8b262b232
nirmaldhakal2003 <nirmalbsccsit2@gmail.com>
2024-08-06 08:40:08 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let name = "Nirmal Dhakal";
+let age = 21;
+let student = true;
+
+document.getElementById("p1").innerHTML = "Hello " + name;
+document.getElementById("p2").innerHTML = "you are " + age + " years old";
+document.getElementById("p3").innerHTML = "Enrolled:" + student;
